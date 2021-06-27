96e91b0beca61cceb26b395a5d7e533131cc6368
juuuun_b  <gudwns5220@gmail.com>
2021-06-27 19:54:01 +0900

    Create 04. [1차] 프렌즈4블록.py

---
+++ Kakao_blind_dump_/01. First/01. 2018 카카오 블라인드 테스트/04. [1차] 프렌즈4블록.py
@@ -0,0 +1,10 @@
+def solution(m, n, board):
+    answer = 0
+    return answer
+
+
+# 14
+print(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"]))
+
+# 15
+print(solution(6, 6, ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]))
