5036bdfd1ce579ffb4db255b2f21884211e7a55e
juuuun_b  <gudwns5220@gmail.com>
2021-07-01 21:04:48 +0900

    Create [1차] 다트 게임.py

---
+++ Kakao_blind_dump_/01. First/01. 2018 카카오 블라인드 테스트/[1차] 다트 게임.py
@@ -0,0 +1,25 @@
+def solution(dartResult):
+    answer = 0
+    return
+
+
+# 37
+print(solution("1S2D*3T"))
+
+# 9
+print(solution("1D2S#10S"))
+
+# 3
+print(solution("1D2S0T"))
+
+# 23
+print(solution("1S*2T*3S"))
+
+# 5
+print(solution("1D#2S*3S"))
+
+# -4
+print(solution("1T2D3D#"))
+
+# 59
+print(solution("1D2S3T*"))
