e080b44448a2e6d6f0f3b694c580c61efe8cbb52
juuuun_b  <gudwns5220@gmail.com>
2021-07-04 16:05:17 +0900

    Update 09. [3차] 압축.py

--- Kakao_blind_dump_/01. First/01. 2018 카카오 블라인드 테스트/09. [3차] 압축.py
+++ Kakao_blind_dump_/01. First/01. 2018 카카오 블라인드 테스트/09. [3차] 압축.py
@@ -42,14 +42,31 @@ def solution(msg):
     return answer
 
 
+def solution2(msg):
+    answer = []
 
+    base_dict = {chr(e + 64): e for e in range(1, 27)}
+    num = 27
+
+    while msg:
+        t = 1
+        while msg[:t] in base_dict.keys() and t <= len(msg):
+            t += 1
+        t -= 1
+
+        answer.append(base_dict[msg[:t]])
+        base_dict[msg[:t + 1]] = num
+        num += 1
+        msg = msg[t:]
+
+    return answer
 
 
 # [11, 1, 27, 15]
-print(solution("KAKAO"))
+print(solution2("KAKAO"))
 
 # [20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34]
-print(solution("TOBEORNOTTOBEORTOBEORNOT"))
+print(solution2("TOBEORNOTTOBEORTOBEORNOT"))
 
 # [1, 2, 27, 29, 28, 31, 30]
-print(solution("ABABABABABABABAB"))
+print(solution2("ABABABABABABABAB"))
