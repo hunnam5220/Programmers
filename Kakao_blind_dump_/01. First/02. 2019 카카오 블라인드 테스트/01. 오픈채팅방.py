d4213c20a6551bb03c6fed7986147dd865de799d
juuuun_b  <gudwns5220@gmail.com>
2021-07-06 01:59:12 +0900

    Update 01. 오픈채팅방.py

--- Kakao_blind_dump_/01. First/02. 2019 카카오 블라인드 테스트/01. 오픈채팅방.py
+++ Kakao_blind_dump_/01. First/02. 2019 카카오 블라인드 테스트/01. 오픈채팅방.py
@@ -1,45 +1,27 @@
 def solution(record):
-    answer, idx = [], 0
+    result = []
+    answer = []
     status = ["님이 들어왔습니다.", "님이 나갔습니다."]
-    visited = {}
     id_code = {}
+
     for r in record:
-        r = r.split()
+        l = r.split()
 
-        if r[0] == 'Enter':
-            if r[1] in visited:
+        if l[0] == 'Enter':
+            id_code[l[1]] = l[2]
+            result.append([0, l[1]])
 
-                for i in visited[r[1]]:
-                    i = int(i)
-                    answer[i] = r[2] + "님이" + answer[i].split("님이")[1]
+        elif l[0] == 'Leave':
+            result.append([1, l[1]])
 
-                id_code[r[1]] = r[2]
-                visited[r[1]] += str(idx)
-                answer.append(r[2] + status[0])
-                idx += 1
+        else:
+            id_code[l[1]] = l[2]
 
-            else:
-                visited[r[1]] = ''
-                visited[r[1]] += str(idx)
-
-                id_code[r[1]] = r[2]
-
-                answer.append(r[2] + status[0])
-                idx += 1
-
-        elif r[0] == 'Leave':
-            answer.append(id_code[r[1]] + status[1])
-            visited[r[1]] += str(idx)
-            idx += 1
-
-        elif r[0] == 'Change':
-            for i in visited[r[1]]:
-                j = int(i)
-                answer[j] = r[2] + "님이" + answer[j].split("님이")[1]
-                id_code[r[1]] = r[2]
+    for s, uid in result:
+        answer.append(id_code[uid] + status[s])
 
     return answer
 
 
 print(solution(
-    ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]))
+    ["Enter uid1234 Muzi",  "Leave uid1234", "Enter uid4567 Prodo", "Enter uid1234 Ryan", "Change uid4567 Prodo"]))
