bdc2c3440df34db3e617b566ca327e941146383f
juuuun_b  <gudwns5220@gmail.com>
2021-07-06 18:14:30 +0900

    Create 02. 실패율.py

---
+++ Kakao_blind_dump_/01. First/02. 2019 카카오 블라인드 테스트/02. 실패율.py
@@ -0,0 +1,10 @@
+def solution(N, stages):
+    answer = []
+    return answer
+
+
+# [3, 4, 2, 1, 5]
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+
+# [4, 1, 2, 3]
+print(solution(4, [4, 4, 4, 4, 4]))
