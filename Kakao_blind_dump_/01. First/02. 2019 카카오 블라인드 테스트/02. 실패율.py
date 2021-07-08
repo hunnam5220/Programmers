54e1773a9f6a597266b76df14a09e267b4edcd28
juuuun_b  <gudwns5220@gmail.com>
2021-07-08 15:19:03 +0900

    Update 02. 실패율.py

--- Kakao_blind_dump_/01. First/02. 2019 카카오 블라인드 테스트/02. 실패율.py
+++ Kakao_blind_dump_/01. First/02. 2019 카카오 블라인드 테스트/02. 실패율.py
@@ -1,5 +1,20 @@
 def solution(N, stages):
-    answer = []
+    result = []
+    total = len(stages)
+    for stage in range(1, N + 1):
+        cnt = stages.count(stage)
+        if cnt == 0 and total == 0:
+            fail_per = 0
+        else:
+            fail_per = cnt / total
+
+        result.append((fail_per, stage))
+
+        total -= cnt
+    result.sort(key=lambda x: (-x[0], x[1]))
+
+    answer = [i[1] for i in result]
+
     return answer
 
 
