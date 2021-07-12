b0d4d2b21b4ae734bfdb8ff22c33ffd964f0701e
juuuun_b  <gudwns5220@gmail.com>
2021-07-12 22:20:58 +0900

    Update 07. 블록게임.py

--- Kakao_blind_dump_/01. First/02. 2019 카카오 블라인드 테스트/07. 블록게임.py
+++ Kakao_blind_dump_/01. First/02. 2019 카카오 블라인드 테스트/07. 블록게임.py
@@ -1,73 +1,8 @@
-mv = [-2, -1, 0, 1, 2]
+mv1 = [-2, -1, 0]
+mv2 = [0, 1, 2]
 
 
-def rm_block(base, board, num):
-
-    for a, b in base:
-        idx = 0
-        board[a][b] = '#'
-        while 1:
-            if a + idx >= len(board):
-                break
-
-            if board[a + idx][b] == num or board[a + idx][b] == 0:
-                board[a + idx][b] = '#'
-                idx += 1
-            else:
-                break
-
-
-def check_block(board, block, n, answer):
-    rere = False
-
-    for num in block:
-        for x in range(n - 1, -1, -1):
-            for y in range(n):
-                base = []
-                if board[x][y] == num:
-                    for k in mv:
-                        if -1 < y + k < n and board[x][y + k] == num:
-                            base.append((x, y + k))
-
-                if len(base) < 2:
-                    continue
-
-                elif len(base) == 3:
-                    tmp = []
-                    for k in [-1, 1]:
-                        if -1 < base[0][0] + k < n:
-                            for a, b in base:
-                                if board[base[0][0] + k][b] == '#' or board[base[0][0] + k][b] == num:
-                                    tmp.append((base[0][0] + k, b))
-                    base.extend(tmp)
-                else:
-                    tmp = []
-                    for k in [-2, -1, 1, 2]:
-                        if -1 < base[0][0] + k < n:
-                            for a, b in base:
-                                if board[base[0][0] + k][b] == '#' or board[base[0][0] + k][b] == num:
-                                    tmp.append((base[0][0] + k, b))
-                    base.extend(tmp)
-
-                if len(base) == 6:
-                    rere = True
-                    rm_block(base, board, num)
-                    answer += 1
-
-    return rere, answer
-
-
-def solution(board):
-    answer = 0
-    block = []
-    for a in board:
-        for b in a:
-            if b != 0 and b not in block:
-                block.append(b)
-
-    block.sort()
-    n = len(board)
-
+def fill(n, board):
     for x in range(n):
         for y in range(n):
             if board[y][x] == 0:
@@ -79,8 +14,95 @@ def solution(board):
             else:
                 break
 
+
+# 블럭 지우자
+def rm_block(floor, board):
+    for a, b in floor:
+        if board[a][b] != '#':
+            board[a][b] = 0
+
+    fill(len(board), board)
+
+
+# 블럭 검사
+def check_block(board, n, answer):
+    rere = False
+    ht = 0
+    # 맨 아래부터 검사
+    for x in range(n - 1, -1, -1):
+        for y in range(n):
+            # 맨 바닥을 긁어 세칸인지 두칸인지 검사
+            floor = []
+
+            # 맨 바닥이 0이 아니고 '#'이 아니면
+            if board[x][y] != 0 and board[x][y] != '#':
+                cnt = 0
+                # 현재 칸 x 기준 x-2 ~ x 까지 검사
+                for k in mv1:
+                    if -1 < y + k < n and board[x][y + k] == board[x][y]:
+                        floor.append((x, y + k))
+                        cnt += 1
+
+                # 위 검사에서 세칸 이상 안됐으면 우측을 검사
+                # 현재 칸 x 기준 x ~ x + 2 까지 검사
+                if cnt < 2:
+                    cnt = 0
+                    floor = []
+                    for k in mv2:
+                        if -1 < y + k < n and board[x][y + k] == board[x][y]:
+                            floor.append((x, y + k))
+                            cnt += 1
+
+            # 바닥의 수가 2칸 미만이면 어차피 못깨뜨림
+            if len(floor) < 2:
+                continue
+
+            # 바닥의 수가 3칸이면
+            if len(floor) == 3:
+                tmp = []
+                # 위 아래로 2*3 여섯칸이 돠나요?
+                for k in [-1, 1]:
+                    if -1 < floor[0][0] + k < n:
+                        for a, b in floor:
+                            # 조건에 맞으면 tmp에 넣어라
+                            if board[floor[0][0] + k][b] == '#':
+                                tmp.append((floor[0][0] + k, b))
+                                ht += 1
+
+                            if board[floor[0][0] + k][b] == board[x][y]:
+                                tmp.append((floor[0][0] + k, b))
+            else:
+                tmp = []
+                for k in [-2, -1, 1, 2]:
+                    if -1 < floor[0][0] + k < n:
+                        for a, b in floor:
+                            if board[floor[0][0] + k][b] == '#':
+                                tmp.append((floor[0][0] + k, b))
+                                ht += 1
+
+                            if board[floor[0][0] + k][b] == board[x][y]:
+                                tmp.append((floor[0][0] + k, b))
+
+            floor.extend(tmp)
+
+            if len(floor) == 6:
+                rere = True
+                rm_block(floor, board)
+                answer += 1
+
+    return rere, answer
+
+
+def solution(board):
+    answer = 0
+    n = len(board)
+
+    # 0으로 된 빈공간을 위부터 떨어지는 형시으로 #으로 채워놓고
+    fill(n, board)
+
     while 1:
-        r, answer = check_block(board, block, n, answer)
+        # 검사해서 지우기
+        r, answer = check_block(board, n, answer)
         if not r:
             break
 
@@ -107,6 +129,3 @@ print(solution([[0, 0, 0, 0, 0], [1, 0, 0, 2, 0], [1, 2, 2, 2, 0], [1, 1, 0, 0, 
 
 # 0
 print(solution([[0, 0, 1, 1, 1], [0, 0, 0, 1, 0], [3, 0, 0, 2, 0], [3, 2, 2, 2, 0], [3, 3, 0, 0, 0]]))
-
-# 1
-print(solution([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 2, 2, 0, 0, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
